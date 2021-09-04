3bf2442197d7f11761118b3fd36b42835c4de3f2
PhotonQuantum <self@lightquantum.me>
2021-09-05 04:09:23 +0800

    feat(request): add join & map combinators

--- src/lib/request.rs
+++ src/lib/request.rs
@@ -7,11 +7,11 @@ use std::task::{Context, Poll};
 use actix::dev::{Request, ToEnvelope};
 use actix::MailboxError;
 use actix::{Actor, Addr, Handler, Message};
-use futures::future::JoinAll;
+use futures::future::{Join, JoinAll};
 use futures::ready;
 use pin_project::pin_project;
 
-trait RequestTrait: Future {
+pub trait RequestTrait: Future {
     /// Sends messages unconditionally, bypassing mailbox limit and ignore its response.
     fn immediately(self);
     /// Sends messages with blocking.
@@ -19,42 +19,157 @@ trait RequestTrait: Future {
     /// # Panics
     /// Panics when called in an `actix-rt`(`tokio`) context, or any `MsgRequest` has already been polled halfway.
     fn blocking(self) -> <Self as Future>::Output;
+
+    fn join<R: RequestTrait + Sized>(self, other: R) -> MsgRequestTuple<Self, R>
+    where
+        Self: Sized,
+    {
+        MsgRequestTuple::new(self, other)
+    }
+
+    fn map<O, F: FnOnce(Self::Output) -> O>(self, f: F) -> MsgRequestMap<Self, O, F>
+    where
+        Self: Sized,
+    {
+        MsgRequestMap::new(self, f)
+    }
 }
 
 #[must_use = "You have to await on requests, call `blocking()` or `immediately()`, otherwise the message wont be delivered"]
-#[pin_project(project = MsgRequestsProj)]
-pub enum MsgRequests<'a, A, M>
+#[pin_project(project = MsgRequestMapProj)]
+pub enum MsgRequestMap<R, O, F>
 where
-    A: Actor + Handler<M>,
-    A::Context: ToEnvelope<A, M>,
-    M: Message + Send + Unpin + 'static,
-    M::Result: Send,
+    R: Future,
+    F: FnOnce(R::Output) -> O,
 {
-    Init(Vec<MsgRequest<'a, A, M>>),
-    Fut(#[pin] JoinAll<MsgRequest<'a, A, M>>),
+    Init(#[pin] R, Option<F>),
     Gone,
 }
 
-impl<'a, A, M> MsgRequests<'a, A, M>
+impl<R, O, F> MsgRequestMap<R, O, F>
 where
-    A: Actor + Handler<M>,
-    A::Context: ToEnvelope<A, M>,
-    M: Message + Send + Unpin + 'static,
-    M::Result: Send,
+    R: Future,
+    F: FnOnce(R::Output) -> O,
 {
-    pub fn new(req: impl IntoIterator<Item = MsgRequest<'a, A, M>>) -> Self {
+    pub fn new(r: R, f: F) -> Self {
+        Self::Init(r, Some(f))
+    }
+}
+
+impl<R, O, F> RequestTrait for MsgRequestMap<R, O, F>
+where
+    R: RequestTrait,
+    F: FnOnce(R::Output) -> O,
+{
+    fn immediately(self) {
+        if let Self::Init(r, _) = self {
+            r.immediately();
+        }
+    }
+
+    fn blocking(self) -> <Self as Future>::Output {
+        if matches!(self, Self::Init(_, _)) {
+            actix_rt::Runtime::new()
+                .expect("unable to create runtime")
+                .block_on(self)
+        } else {
+            panic!("already been polled halfway")
+        }
+    }
+}
+
+impl<R, O, F> Future for MsgRequestMap<R, O, F>
+where
+    R: Future,
+    F: FnOnce(R::Output) -> O,
+{
+    type Output = O;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        match self.as_mut().project() {
+            MsgRequestMapProj::Init(req, f) => {
+                let res = ready!(req.poll(cx));
+                let f = f.take().unwrap();
+                self.set(Self::Gone); // seal this MsgRequestTuple
+                Poll::Ready(f(res))
+            }
+            MsgRequestMapProj::Gone => panic!("requests done and can't be polled again"),
+        }
+    }
+}
+
+#[must_use = "You have to await on requests, call `blocking()` or `immediately()`, otherwise the message wont be delivered"]
+#[pin_project(project = MsgRequestTupleProj)]
+pub enum MsgRequestTuple<R1: Future, R2: Future> {
+    Init(Option<R1>, Option<R2>),
+    Fut(#[pin] Join<R1, R2>),
+    Gone,
+}
+
+impl<R1: Future, R2: Future> MsgRequestTuple<R1, R2> {
+    pub fn new(r1: R1, r2: R2) -> Self {
+        Self::Init(Some(r1), Some(r2))
+    }
+}
+
+impl<R1: RequestTrait, R2: RequestTrait> RequestTrait for MsgRequestTuple<R1, R2> {
+    fn immediately(self) {
+        if let Self::Init(r1, r2) = self {
+            r1.unwrap().immediately();
+            r2.unwrap().immediately();
+        }
+    }
+
+    fn blocking(self) -> (R1::Output, R2::Output) {
+        if matches!(self, Self::Init(_, _)) {
+            actix_rt::Runtime::new()
+                .expect("unable to create runtime")
+                .block_on(self)
+        } else {
+            panic!("already been polled halfway")
+        }
+    }
+}
+
+impl<R1: RequestTrait, R2: RequestTrait> Future for MsgRequestTuple<R1, R2> {
+    type Output = (R1::Output, R2::Output);
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        match self.as_mut().project() {
+            MsgRequestTupleProj::Init(r1, r2) => {
+                let r1 = r1.take().unwrap();
+                let r2 = r2.take().unwrap();
+                let futs = futures::future::join(r1, r2);
+                self.set(Self::Fut(futs));
+                cx.waker().wake_by_ref(); // can be polled immediately to receive result
+                Poll::Pending
+            }
+            MsgRequestTupleProj::Fut(fut) => {
+                let res = ready!(fut.poll(cx));
+                self.set(Self::Gone); // seal this MsgRequestTuple
+                Poll::Ready(res)
+            }
+            MsgRequestTupleProj::Gone => panic!("requests done and can't be polled again"),
+        }
+    }
+}
+
+#[must_use = "You have to await on requests, call `blocking()` or `immediately()`, otherwise the message wont be delivered"]
+#[pin_project(project = MsgRequestVecProj)]
+pub enum MsgRequestVec<R: Future> {
+    Init(Vec<R>),
+    Fut(#[pin] JoinAll<R>),
+    Gone,
+}
+
+impl<R: Future> MsgRequestVec<R> {
+    pub fn new(req: impl IntoIterator<Item = R>) -> Self {
         Self::Init(req.into_iter().collect())
     }
 }
 
-impl<'a, A, M> Add for MsgRequests<'a, A, M>
-where
-    A: Actor + Handler<M>,
-    A::Context: ToEnvelope<A, M>,
-    M: Message + Send + Unpin + 'static,
-    M::Result: Send,
-{
-    type Output = MsgRequests<'a, A, M>;
+impl<R: Future> Add for MsgRequestVec<R> {
+    type Output = MsgRequestVec<R>;
 
     fn add(self, rhs: Self) -> Self::Output {
         if let (Self::Init(mut this), Self::Init(rhs)) = (self, rhs) {
@@ -66,16 +181,10 @@ where
     }
 }
 
-impl<'a, A, M> Add<MsgRequest<'a, A, M>> for MsgRequests<'a, A, M>
-where
-    A: Actor + Handler<M>,
-    A::Context: ToEnvelope<A, M>,
-    M: Message + Send + Unpin + 'static,
-    M::Result: Send,
-{
-    type Output = MsgRequests<'a, A, M>;
+impl<R: Future> Add<R> for MsgRequestVec<R> {
+    type Output = MsgRequestVec<R>;
 
-    fn add(self, rhs: MsgRequest<'a, A, M>) -> Self::Output {
+    fn add(self, rhs: R) -> Self::Output {
         if let Self::Init(mut this) = self {
             this.push(rhs);
             Self::Init(this)
@@ -85,13 +194,7 @@ where
     }
 }
 
-impl<'a, A, M, O> RequestTrait for MsgRequests<'a, A, M>
-where
-    A: Actor + Handler<M>,
-    A::Context: ToEnvelope<A, M>,
-    M: Message<Result = O> + Send + Unpin + 'static,
-    O: Send,
-{
+impl<R: RequestTrait> RequestTrait for MsgRequestVec<R> {
     fn immediately(self) {
         if let Self::Init(l) = self {
             l.into_iter().for_each(RequestTrait::immediately);
@@ -99,7 +202,7 @@ where
     }
 
     #[allow(clippy::missing_errors_doc)]
-    fn blocking(self) -> Vec<Result<O, MailboxError>> {
+    fn blocking(self) -> Vec<R::Output> {
         if matches!(self, Self::Init(_)) {
             actix_rt::Runtime::new()
                 .expect("unable to create runtime")
@@ -110,30 +213,24 @@ where
     }
 }
 
-impl<'a, A, M, O> Future for MsgRequests<'a, A, M>
-where
-    A: Actor + Handler<M>,
-    A::Context: ToEnvelope<A, M>,
-    M: Message<Result = O> + Send + Unpin,
-    O: Send,
-{
-    type Output = Vec<Result<O, MailboxError>>;
+impl<R: RequestTrait> Future for MsgRequestVec<R> {
+    type Output = Vec<R::Output>;
 
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         match self.as_mut().project() {
-            MsgRequestsProj::Init(reqs) => {
+            MsgRequestVecProj::Init(reqs) => {
                 let reqs = mem::take(reqs);
                 let futs = futures::future::join_all(reqs);
                 self.set(Self::Fut(futs));
                 cx.waker().wake_by_ref(); // can be polled immediately to receive result
                 Poll::Pending
             }
-            MsgRequestsProj::Fut(fut) => {
+            MsgRequestVecProj::Fut(fut) => {
                 let res = ready!(fut.poll(cx));
-                self.set(Self::Gone); // seal this MsgRequests
+                self.set(Self::Gone); // seal this MsgRequestVec
                 Poll::Ready(res)
             }
-            MsgRequestsProj::Gone => panic!("requests done and can't be polled again"),
+            MsgRequestVecProj::Gone => panic!("requests done and can't be polled again"),
         }
     }
 }
@@ -222,13 +319,14 @@ where
 
 #[cfg(test)]
 mod tests {
-    use std::pin::Pin;
     use std::sync::mpsc::channel;
     use std::thread;
 
     use actix::{Actor, Context, Handler, Message, System};
 
-    use super::{MsgRequest, MsgRequests, RequestTrait};
+    use crate::request::MsgRequestTuple;
+
+    use super::{MsgRequest, MsgRequestVec, RequestTrait};
 
     #[derive(Debug, Message)]
     #[rtype("usize")]
@@ -303,11 +401,11 @@ mod tests {
     }
 
     #[actix::test]
-    async fn must_reqs_do() {
+    async fn must_req_vec_do() {
         let addr1 = Echo::default().start();
         let addr2 = Echo::default().start();
         let addr3 = Echo::default().start();
-        let reqs = MsgRequests::new([
+        let reqs = MsgRequestVec::new([
             MsgRequest::new(&addr1, Ping(41, false)),
             MsgRequest::new(&addr2, Ping(42, false)),
             MsgRequest::new(&addr3, Ping(43, false)),
@@ -332,9 +430,9 @@ mod tests {
     }
 
     #[actix::test]
-    async fn must_reqs_await() {
+    async fn must_req_vec_await() {
         let addr = Echo::default().start();
-        let reqs = MsgRequests::new([
+        let reqs = MsgRequestVec::new([
             MsgRequest::new(&addr, Ping(41, false)),
             MsgRequest::new(&addr, Ping(42, false)),
             MsgRequest::new(&addr, Ping(43, false)),
@@ -350,7 +448,7 @@ mod tests {
     }
 
     #[test]
-    fn must_reqs_blocking() {
+    fn must_req_vec_blocking() {
         let (tx, rx) = channel();
 
         // spawn in a new thread to avoid blocking main thread
@@ -364,7 +462,7 @@ mod tests {
         });
 
         let addr = rx.recv().unwrap();
-        let reqs = MsgRequests::new([MsgRequest::new(&addr, Ping(42, true))]);
+        let reqs = MsgRequestVec::new([MsgRequest::new(&addr, Ping(42, true))]);
         assert_eq!(
             reqs.blocking()
                 .into_iter()
@@ -375,5 +473,39 @@ mod tests {
         );
 
         sys_thread.join().unwrap(); // sys thread should join and mustn't panic
+    }
+
+    #[actix::test]
+    async fn must_req_tuple_do() {
+        let addr1 = Echo::default().start();
+        let addr2 = Echo::default().start();
+        let reqs = MsgRequestTuple::new(
+            MsgRequest::new(&addr1, Ping(41, false)),
+            MsgRequest::new(&addr2, Ping(42, false)),
+        );
+        reqs.immediately();
+        tokio::task::yield_now().await;
+        assert_eq!(
+            addr1.send(Query).await.unwrap(),
+            41,
+            "message not delivered"
+        );
+        assert_eq!(
+            addr2.send(Query).await.unwrap(),
+            42,
+            "message not delivered"
+        );
+    }
+
+    #[actix::test]
+    async fn must_req_tuple_await() {
+        let addr = Echo::default().start();
+        let reqs = MsgRequestTuple::new(
+            MsgRequest::new(&addr, Ping(41, false)),
+            MsgRequest::new(&addr, Ping(42, false)),
+        );
+        let res = reqs.await;
+        assert_eq!(reqs.0.unwrap(), 41, "response incorrect");
+        assert_eq!(reqs.1.unwrap(), 41, "response incorrect");
     }
 }
